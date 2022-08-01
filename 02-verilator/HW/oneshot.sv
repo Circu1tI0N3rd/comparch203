acfae3f9f2e300a96060d3cf39380f7931564780
Maurice Klivoslov <thanhhaidoan@tutanota.com>
2022-08-01 10:34:42 +0700

    A solution to the given HW on 30/07/2022.
    A stable pulse begins at the matching posedge and lasts till the next posedge.
    Outputing stable pulse during clock high requires rewriting testbench to monitor
    responses at clock negedge.
    
    Adapted for macOS GCC, Make and Verilator.

---
+++ 02-verilator/HW/oneshot.sv
@@ -0,0 +1,51 @@
+module oneshot(
+  input  wire clk_i,
+  input  wire rstn_i,
+  input  wire btn_i,
+  output wire stb_o
+  );
+
+reg [1:0] state;
+reg       stable;
+
+assign stb_o = stable;
+
+always @(posedge clk_i)
+  begin
+    if (rstn_i == 1'b0)
+    begin
+      state  <= 0;
+      stable <= 0;
+    end
+    else
+    begin
+      case (state)
+        0 : /* IDLE */
+          if (btn_i)
+          begin
+            state  <= state + 1;
+            stable <= 1;
+          end
+        1 : /* PRESS */
+        begin
+          stable <= 0;
+          if (btn_i)
+            state <= state + 1;
+          else
+            state <= 0;
+        end
+        2 : /* HOLD */
+        begin
+          if (!btn_i)
+            state <= 0;
+        end
+        default : /* default */
+        begin
+          state  <= 0;
+          stable <= 0;
+        end
+      endcase
+    end
+  end
+
+endmodule // debounce
---
+++ 02-verilator/HW/tb_oneshot.cpp
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+#include <verilated_fst_c.h>
+#include "Voneshot.h"
+
+#define MAX_SIM       100
+vluint64_t sim_unit = 0;
+vluint64_t sim_time = 0;
+
+uint8_t btn_state = 0;
+uint8_t stable    = 0;
+uint8_t stable_q  = 0;
+
+void dut_clock(Voneshot *dut, VerilatedFstC *vtrace);
+void set_reset(Voneshot *dut);
+void set_random(Voneshot *dut);
+void get_expected(Voneshot *dut);
+void monitor_proc(Voneshot *dut);
+void monitor_outputs(Voneshot *dut);
+
+void initial(Voneshot *dut) {
+}
+
+void dut_clock(Voneshot *dut, VerilatedFstC *vtrace) {
+  sim_time = sim_unit * 10 + 2;
+  if (vtrace)
+    vtrace->dump(sim_time); // Dump values to update inputs
+
+  sim_time = sim_time + 3;
+  dut->clk_i = 0;
+  dut->eval();
+  if (vtrace)
+    vtrace->dump(sim_time); // Dump values after negedge
+
+  monitor_outputs(dut);        // Values are "stable" to monitor
+
+  sim_time = sim_time + 5;
+  dut->clk_i = 1;
+  dut->eval();
+  if (vtrace) {
+    vtrace->dump(sim_time); // Dump values after posedge
+    //vtrace->flush();
+  }
+}
+
+void set_reset(Voneshot *dut) {
+  if ((sim_unit < 5) || (sim_unit > 10))
+    dut->rstn_i = 1;
+  else
+    dut->rstn_i = 0;
+}
+
+void set_random(Voneshot *dut) {
+  dut->btn_i = rand()%2;
+  dut->eval();
+}
+
+void get_expected(Voneshot *dut) {
+  if (dut->rstn_i)
+    if (dut->btn_i)
+      if (btn_state)
+        stable = 0;
+      else {
+        btn_state = 1;
+        stable = 1;
+      }
+    else {
+      btn_state = 0;
+      stable = 0;
+    }
+  else {
+    btn_state = 0;
+    stable = 0;
+  }
+}
+
+void monitor_proc(Voneshot *dut) {
+  // One FSM DFF
+  if (dut->rstn_i)
+    stable_q = stable;
+  else
+    stable_q = 0;
+}
+
+void monitor_outputs(Voneshot *dut) {
+  printf("@%3d: ", sim_unit);
+  printf("RECV %1x : ", dut->stb_o);
+  printf("EXPE %1x ", stable_q);
+
+  if (stable_q == dut->stb_o)
+    printf("\n");
+  else
+    printf("<= FAIL\n");
+}
+
+int main(int argc, char **argv, char **env) {
+	// Call commandArgs first!
+	Verilated::commandArgs(argc, argv);
+
+  // Instantiate the design
+	Voneshot *dut = new Voneshot;
+
+  // Trace generating
+  Verilated::traceEverOn(true);
+  VerilatedFstC *vtrace = new VerilatedFstC;
+  dut->trace(vtrace, 2); // trace down to 2 hierarchy
+  vtrace->open("oneshot.fst");
+  vtrace->dump(0);
+
+  // Initial setups
+  srand(time(NULL));
+  initial(dut);
+  dut->eval();
+
+  // Check procedure
+  while (sim_unit < MAX_SIM){
+    dut_clock(dut, vtrace);
+    set_random(dut);
+    monitor_proc(dut);
+    get_expected(dut);
+    set_reset(dut);
+    sim_unit++;
+	}
+
+  vtrace->close();
+  delete dut;
+  exit(EXIT_SUCCESS);
+}
